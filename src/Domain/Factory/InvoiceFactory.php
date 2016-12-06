91696f70dfe6bd5df9808e6a38760bca655aaa9f
larry <ladams1776@yahoo.com>
2016-12-05 23:03:31 -0500

    Filling in InvoiceFactory

--- src/Domain/Factory/InvoiceFactory.php
+++ src/Domain/Factory/InvoiceFactory.php
@@ -14,6 +14,11 @@ class InvoiceFactory
 {
     public function createFromOrder(Order $order)
     {
-        return new Invoice();
+        $invoice = new Invoice();
+        $invoice->setOrder($order);
+        $invoice->setInvoiceDate(new \DateTime());
+        $invoice->setTotal($order->getTotal());
+        
+        return $invoice;        
     }
 }
