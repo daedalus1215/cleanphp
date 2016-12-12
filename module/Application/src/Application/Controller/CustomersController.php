a417fe5e54000029c2891ff0064ad7e633ed9385
larry <ladams1776@yahoo.com>
2016-12-11 20:53:08 -0500

    added flash message to newAction

--- module/Application/src/Application/Controller/CustomersController.php
+++ module/Application/src/Application/Controller/CustomersController.php
@@ -1,10 +1,12 @@
 <?php
 namespace Application\Controller;
 
+use CleanPhp\Invoicer\Domain\Entity\Customer;
 use CleanPhp\Invoicer\Domain\Repository\CustomerRepositoryInterface;
 use CleanPhp\Invoicer\Service\InputFilter\CustomerInputFilter;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Stdlib\Hydrator\HydratorInterface;
+use Zend\View\Model\ViewModel;
 
 /**
  * Description of CustomersController
@@ -37,17 +39,28 @@ class CustomersController extends AbstractActionController
     
     public function newAction()
     {
+        $viewModel = new ViewModel();
+        $customer = new Customer();
+        
         if ($this->getRequest()->isPost()) {
             $this->inputFilter->setData($this->params()->fromPost());            
+            
             if ($this->inputFilter->isValid()) {
+                // Populate Customer object
                 $customer = $this->hydrator->hydrate($this->inputFilter->getValues(), new Customer());
-                $this->customerRepository->begin()->persist($customer)->commit();
-                
+                // Save Customer
+                $this->customerRepository->begin()->persist($customer)->commit();                
+                // Message Success
                 $this->flashMessenger()->addSuccessMessage('Customer Saved');
+                // Kick back to customers page.
                 $this->redirect()->toUrl('/customers');
             } else {
-                
+                $this->hydrator->hydrate($this->params()->fromPost(), $customer);
             }
         }
+        
+        $viewModel->setVariable('customer', $customer);
+        
+        return $viewModel;
     }
 }
